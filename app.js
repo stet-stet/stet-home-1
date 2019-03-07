ae5a465fabf1088ff5350f33a8af33c73e899e2f
stet_stet <jayeonyi@snu.ac.kr>
2019-03-07 21:36:42 +0900

    refreshing

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var ejs = require('ejs');
-const {textData} = require("./assets/states");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,9 +10,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,11 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-const ejsOptions = {};
-app.engine('ejs',(path, data, callback)=>{
-  ejs.renderFile(path, textData, ejsOptions, callback);
-})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const {textData} = require("../assets/states");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(textData);
-  res.render("index.ejs",textData);
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
