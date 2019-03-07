f1f960877ff14753cb3a91c9c30b615041c9edef
stet_stet <jayeonyi@snu.ac.kr>
2019-03-08 00:32:03 +0900

    hello

--- assets/states.js
+++ assets/states.js
@@ -3,18 +3,11 @@ const ejs = require("ejs")
 categoryNames=["Profile","Works","Contact"];
 
 exports.textData = {
-    title:"This is an example title",
-    category:[
-      {name:categoryNames[0],content:`<a href="">${categoryNames[0]}</a>`},
-      {name:categoryNames[1],content:`${categoryNames[1]}`},
-      {name:categoryNames[2], content:`${categoryNames[2]}`},
-    ],
-    logo:"Information alone is not enough; we also need the MEANING of that information.",
-    content:"Lorem ipsum",
-    health:{
-      hungry:false,
-      angry:false,
-      lonely:false,
-      tired:false,
-    }
-  };
+    title:"Stet's HALT status",
+    status:[
+      {name:"hungry", letter:"H", state: true},
+      {name:"angry", letter:"A", state: false},
+      {name:"lonely", letter:"L", state: false},
+      {name:"tired", letter:"T", state: false},
+    ]
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
+var textData = require('../assets/states').textData;
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', textData);
 });
 
+router.get('/:keyword', function(req,res,next){
+  var n = {
+    title: textData.title,
+    status:[],
+  }
+  for(i of textData.status){
+    if(req.params.keyword.search(i["letter"]) !== -1){
+      n.status.push({name:i.name, letter:i.letter, state: true});
+    } else {
+      n.status.push({name:i.name, letter:i.letter, state: false});
+    }
+  }
+  textData = n;
+  res.redirect('/');
+})
+
+router.get('/reset',function(req,res,next){
+  textData = {
+    title:"Stet's HALT status",
+    status:[
+      {name:"hungry", letter:"H", state: false},
+      {name:"angry", letter:"A", state: false},
+      {name:"lonely", letter:"L", state: false},
+      {name:"tired", letter:"T", state: false},
+    ],
+  }
+})
 module.exports = router;
